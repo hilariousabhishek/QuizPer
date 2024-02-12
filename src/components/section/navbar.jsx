c934498d1eeb7ccdb505b8c3581604d7fcf7e845
Abhishek Kumar <Abhishek.ankush21@gmail.com>
2024-02-12 23:01:37 +0530

    Add files via upload

--- src/components/section/navbar.jsx
+++ src/components/section/navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="/Quiz">Quiz <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="/current.html">Quiz <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="/">Link</a>
